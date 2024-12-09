989e8af2e11a59fccea3ce58d916e4eef94d727b
Halan Vinicius <halanm41@gmail.com>
2024-12-09 20:43:44 -0300

    Create home.py

---
+++ home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+
+# Configuração do título
+st.set_page_config(page_title="Home", layout="centered")
+
+# Página inicial
+st.title("Bem-vindo ao Hub de Projetos")
+st.markdown("Escolha um dos projetos abaixo para começar:")
+
+# Menu de navegação
+project_options = {
+    "Projeto 1": "pages/project1.py",
+    "Projeto 2": "pages/project2.py",
+}
+
+# Seleção do projeto
+project_selected = st.selectbox("Selecione um projeto:", ["Selecione"] + list(project_options.keys()))
+
+if project_selected != "Selecione":
+    script_path = project_options[project_selected]
+    with open(script_path) as f:
+        exec(f.read())  # Executa o script do projeto selecionado
