372d264e2973b590d6aa49402343fbac017620ea
Halan Vinicius <halanm41@gmail.com>
2024-12-09 20:56:58 -0300

    Update home.py

--- home.py
+++ home.py
@@ -13,16 +13,16 @@ st.markdown("Escolha um dos projetos abaixo para começar:")
 
 # Menu de navegação
 project_options = {
-    "Projeto 1": "projeto1.py",  # Caminho do arquivo
-    "Projeto 2": "projeto2.py",
+    "Projeto 1": "pagina1",  # Nome do arquivo sem a extensão .py
+    "Projeto 2": "pagina2",
 }
 
 # Seleção do projeto
 project_selected = st.selectbox("Selecione um projeto:", ["Selecione"] + list(project_options.keys()))
 
 if project_selected != "Selecione":
-    script_name = project_options[project_selected]
-    script_path = os.path.join(scripts_dir, script_name)
+    script_name = project_options[project_selected]  # Nome do arquivo sem extensão
+    script_path = os.path.join(scripts_dir, f"{script_name}.py")  # Adiciona a extensão .py ao nome do arquivo
     
     # Verificando se o arquivo existe
     if os.path.exists(script_path):
@@ -32,3 +32,4 @@ if project_selected != "Selecione":
             st.code(code, language="python")  # Exibe o conteúdo do script no Streamlit
     else:
         st.error(f"Erro: O arquivo do {project_selected} não foi encontrado.")
+        st.write(f"Caminho procurado: {script_path}")  # Exibe o caminho para depuração
